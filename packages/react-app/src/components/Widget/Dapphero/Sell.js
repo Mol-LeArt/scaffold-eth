240a21f3004b44d2a697cee27f91ec912254131e
Taki <tk@noubies.com>
2020-09-25 15:26:28 +0100

    :arrow_up: Add mvp libraries + ethers@v5

--- packages/react-app/src/components/Widget/Dapphero/Sell.js
+++ packages/react-app/src/components/Widget/Dapphero/Sell.js
@@ -14,7 +14,7 @@ function Sell() {
   const approve = async (addressOfToken, tokenId) => {
       // A Web3Provider wraps a standard Web3 provider, which is
       // what Metamask injects as window.ethereum into each page
-      const provider = new ethers.providers.Web3Provider(web3.currentProvider)
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
     
       // The Metamask plugin also allows signing transactions to
       // send ether and pay to change state within the blockchain.
