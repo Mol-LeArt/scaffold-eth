a43c6d50e9e8547b5916572642531fa66ec180ec
Taki <tk@noubies.com>
2020-09-27 23:29:57 +0100

    :lipstick: Add decentraland-ui as mvp

--- packages/react-app/src/components/Header/Header.js
+++ packages/react-app/src/components/Header/Header.js
@@ -1,21 +1,20 @@
 
 import React from 'react';
-import clsx from 'clsx';
 import styles from './styles.module.scss';
+
+import { Hero } from 'decentraland-ui'
 
 function Header() {
     return (
-      <header className={clsx('hero hero--primary', styles.hero)}>
-        <img className={clsx('hero__image', styles.heroImage)} src="img/hero.png"/>
-        <div className="container" style={{ position: "relative"}}>
-          <h1 className={clsx('hero__title', styles.heroTitle)}>Mol LeArt</h1>
-        </div>
-        <div style={{ position: "absolute", top: 0, right: 0, padding: 10 }}>
-          <button className="button button--secondary" data-dh-feature="network" data-dh-property-enable="true">
-              Connect your account
-          </button>
-        </div>
-      </header>
+      <div className="story">
+        <Hero centered>
+          <Hero.Header>Mol LeArt</Hero.Header>
+          <Hero.Description>DAO managed creative commons</Hero.Description>
+          <Hero.Content className={styles.heroImage}>
+            <img src="img/hero.png"/>
+          </Hero.Content>
+        </Hero>
+      </div>
     )
 }
 
