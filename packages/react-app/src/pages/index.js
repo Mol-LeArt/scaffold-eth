56982dafb4d10a5c8c5c1f2a789d7042f2650efb
Taki <tk@noubies.com>
2020-09-29 19:27:59 +0100

    :fire: Remove Features & Add menu

--- packages/react-app/src/pages/index.js
+++ packages/react-app/src/pages/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import styles from './styles.module.scss';
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Menu from "./menu/index"
 
 import Fullscreen from '../components/Fullscreen/Fullscreen'
 
 function Home() {
   return (
     <BrowserRouter>
-      <Fullscreen/>
+      <Switch>
+        <Route path="/menu">
+          <Menu />
+        </Route>
+        <Route path="/">
+          <Fullscreen />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
--- packages/react-app/src/pages/menu/index.js
+++ packages/react-app/src/pages/menu/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import styles from './styles.module.scss';
 
-import Features from '../components/Features/Features'
-import Widget from '../components/Widget/Widget'
-import Chart from '../components/Chart/Chart'
-import Footer from '../components/Footer/Footer'
+import Tabs from '../../components/Tabs/Tabs'
+import Widget from '../../components/Widget/Widget'
+import Chart from '../../components/Chart/Chart'
+import Footer from '../../components/Footer/Footer'
 
-import Tabs from '../components/Tabs/Tabs'
-
-function Home() {
+function menu() {
   return (
     <div>
       <Tabs/>
@@ -20,4 +18,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default menu;
