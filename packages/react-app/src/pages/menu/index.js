cf2d8addee951e97a01a59c7bda22db7ee6dcc60
Taki <tk@noubies.com>
2020-09-30 22:53:08 +0100

    :sparkles: Add basic routing function for pages

--- packages/react-app/src/pages/menu/index.js
+++ packages/react-app/src/pages/menu/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import styles from './styles.module.scss';
 
-import Tabs from '../../components/Tabs/Tabs'
-import Widget from '../../components/Widget/Widget'
-import Chart from '../../components/Chart/Chart'
-import Footer from '../../components/Footer/Footer'
+import { Switch, Route, withRouter } from "react-router-dom";
 
-function menu() {
+import Tabs from '../../components/Tabs/Tabs'
+import Footer from '../../components/Footer/Footer'
+import Scribe from "./scribe/index"
+import UpdateSale from "./update-sale/index"
+import Purchase from "./purchase/index"
+import Gift from "./gift/index"
+
+function menu(props) {
   return (
     <div>
       <Tabs/>
-        {/* <Features/> */}
-        {/* <Widget/> */}
-        {/* <Chart/> */}
+      <Switch>
+        <Route path={props.match.url + '/' + 'scribe'} component={Scribe}/>
+        <Route path={props.match.url + '/' + 'update-sale'} component={UpdateSale}/>
+        <Route path={props.match.url + '/' + 'purchase'} component={Purchase}/>
+        <Route path={props.match.url + '/' + 'gift'} component={Gift}/>
+      </Switch>
       <Footer/>
     </div>
   );
 }
 
-export default menu;
+export default withRouter(menu);
---
+++ packages/react-app/src/pages/menu/update-sale/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+import { Header, Page } from 'decentraland-ui'
+
+function scribe() {
+  return (
+    <Page>
+      <Header>Hello Wolrd</Header>
+      <p>This page has a menu</p>
+    </Page>
+  );
+}
+
+export default scribe;
