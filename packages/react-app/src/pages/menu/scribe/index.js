94a61e5ad9ecdad7e4fcb0d8321a1718275fd969
Taki <tk@noubies.com>
2020-09-30 23:42:13 +0100

    :sparkles: Add scribe field

--- packages/react-app/src/pages/menu/scribe/index.js
+++ packages/react-app/src/pages/menu/scribe/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import styles from './styles.module.scss';
 
-import { Header, Page } from 'decentraland-ui'
+import { Page, Field, Form, Button, Radio} from 'decentraland-ui'
 
-function scribe() {
-  return (
-    <Page>
-      <Header>Hello Wolrd</Header>
-      <p>This page has a menu</p>
-    </Page>
-  );
+class Scribe extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: ""
+    };
+  }
+
+  render() {
+    return (
+      <Page>
+        <Form>
+          <Field label="Token address" value={this.state.address} type="address" />
+          <Field label="Token id" />
+          <Field label="Eth price" />
+          <Field label="Starting royalties" />
+          <Field
+            label="Label"
+            value="Input value"
+            action="submit"
+            onAction={() => alert('Action triggered!')}
+          />
+          <Radio toggle label="ON SALE" />
+          <br/><br/><br/>
+          <Button primary>Scribe</Button>
+        </Form>
+      </Page>
+    );
+  }
 }
 
-export default scribe;
+export default Scribe;
